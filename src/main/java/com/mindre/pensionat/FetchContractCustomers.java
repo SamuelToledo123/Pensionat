d6b9dd4adb78b8cfec583e99faad0aaae0c2314c
Samuel_Toledo <samuel.toledo@yh.nackademin.se>
2024-05-08 13:19:31 +0200

    we can now fetch from the list and save in database

---
+++ src/main/java/com/mindre/pensionat/FetchContractCustomers.java
@@ -0,0 +1,62 @@
+package com.mindre.pensionat;
+
+
+import com.fasterxml.jackson.dataformat.xml.JacksonXmlModule;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import com.mindre.pensionat.Models.AllContractCustomers;
+import com.mindre.pensionat.Models.ContractCustomer;
+import com.mindre.pensionat.Repo.ContractCustomerRepo;
+import com.mindre.pensionat.Services.Impl.BookedRoomServiceHtml;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+
+import java.net.URL;
+
+@Component
+@RequiredArgsConstructor
+public class FetchContractCustomers implements CommandLineRunner {
+
+    private final ContractCustomerRepo repo;
+    private static final Logger logger = LoggerFactory.getLogger(FetchContractCustomers.class);
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        try {
+
+            System.out.println("Fetching Contract Customers");
+            JacksonXmlModule module = new JacksonXmlModule();
+            module.setDefaultUseWrapper(false);
+            XmlMapper xmlMapper = new XmlMapper(module);
+            AllContractCustomers allContractCustomers = xmlMapper.readValue(new URL("https://javaintegration.systementor.se/customers"),
+                    AllContractCustomers.class);
+
+            for (ContractCustomer c : allContractCustomers.customers) {
+                System.out.println(c.getId());
+                System.out.println(c.getContactName());
+
+
+                ContractCustomer contractCustomer = new ContractCustomer();
+                contractCustomer.setId(c.getId());
+                contractCustomer.setCompanyName(c.getCompanyName());
+                contractCustomer.setContactName(c.getContactName());
+                contractCustomer.setContactTitle(c.getContactTitle());
+                contractCustomer.setStreetAddress(c.getStreetAddress());
+                contractCustomer.setCity(c.getCity());
+                contractCustomer.setPostalCode(c.getPostalCode());
+                contractCustomer.setCountry(c.getCountry());
+                contractCustomer.setPhone(c.getPhone());
+                contractCustomer.setFax(c.getFax());
+
+                repo.save(contractCustomer);
+
+            }
+        } catch (Exception e) {
+            logger.error("Error while Fetching ContractCustomer" + e);
+        }
+    }
+}
