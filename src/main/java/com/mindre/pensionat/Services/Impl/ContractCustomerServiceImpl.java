26f8e3876693ae9930484b3a9433e2619f506e3f
Samuel_Toledo <samuel.toledo@yh.nackademin.se>
2024-05-10 10:54:30 +0200

    ContractCustomer html page

---
+++ src/main/java/com/mindre/pensionat/Services/Impl/ContractCustomerServiceImpl.java
@@ -0,0 +1,22 @@
+package com.mindre.pensionat.Services.Impl;
+
+import com.mindre.pensionat.Repo.ContractCustomerRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ContractCustomerServiceImpl {
+
+    private final ContractCustomerRepo repo;
+
+    public String getContractCustomers(Model model) {
+        List<com.mindre.pensionat.Models.ContractCustomer> contractCustomers = repo.findAll();
+        model.addAttribute("contractCustomers",contractCustomers);
+        return "contract/ContractCustomers";
+    }
+
+}
