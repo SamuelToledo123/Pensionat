2aab27532c98dafa9c043218376986757945ac0d
Samuel_Toledo <samuel.toledo@yh.nackademin.se>
2024-05-16 16:34:55 +0200

    fixed imports

--- src/main/java/com/mindre/pensionat/Services/Impl/RoomServiceImpl.java
+++ src/main/java/com/mindre/pensionat/Services/Impl/RoomServiceImpl.java
@@ -8,7 +8,6 @@ import com.mindre.pensionat.Services.RoomService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.ui.Model;
 
 import java.util.List;
 
