368dc3ddebd7f28ea4c2f31f5f79c3fec9d5fb38
KareemKDX <drammeh@msn.com>
2024-05-28 12:23:24 +0200

    RoomPrice fixed

--- src/main/java/com/mindre/pensionat/Models/Room.java
+++ src/main/java/com/mindre/pensionat/Models/Room.java
@@ -28,12 +28,13 @@ public class Room {
     @OneToMany(mappedBy = "room" , cascade = CascadeType.ALL)
     private List<BookedRoom> bookedRooms;
 
-    public Room(Long id, String roomType, int roomSize, int amountOfBeds, boolean available) {
+    public Room(Long id, String roomType, int roomSize, int amountOfBeds, double pricePerNight, boolean available) {
         this.id = id;
         this.roomType = roomType;
         this.roomSize = roomSize;
         this.amountOfBeds = amountOfBeds;
         this.available = available;
+        this.pricePerNight = pricePerNight;
         this.bookedRooms = new ArrayList<>();
     }
 
--- src/main/java/com/mindre/pensionat/RoomDataSeeder.java
+++ src/main/java/com/mindre/pensionat/RoomDataSeeder.java
@@ -18,11 +18,11 @@ public class RoomDataSeeder {
     }
 
     public void seed() {
-        Room room1 = new Room(1L, "Single", 1, 1, true );
-        Room room2 = new Room(2L, "Single", 1, 1, true );
-        Room room3 = new Room(3L, "Double", 2, 1, true );
-        Room room4 = new Room(4L, "Double", 2, 2, true );
-        Room room5 = new Room(5L, "Double Luxury", 3, 2, true );
+        Room room1 = new Room(1L, "Single", 1, 1, 499, true );
+        Room room2 = new Room(2L, "Single", 1, 1, 358, true );
+        Room room3 = new Room(3L, "Double", 2, 1, 897, true );
+        Room room4 = new Room(4L, "Double", 2, 2, 1478, true );
+        Room room5 = new Room(5L, "Double Luxury", 3, 2, 1900, true );
 
         roomrepo.save(room1);
         roomrepo.save(room2);
