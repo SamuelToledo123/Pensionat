f0e9f7e91ddf6e61bd338098759d26e12b9f7767
KareemKDX <drammeh@msn.com>
2024-05-29 11:12:27 +0200

    roomservicetest and event added

--- src/test/java/com/mindre/pensionat/Services/RoomServiceTest.java
+++ src/test/java/com/mindre/pensionat/Services/RoomServiceTest.java
@@ -7,6 +7,8 @@ import com.mindre.pensionat.Repo.EventRepo;
 import com.mindre.pensionat.Repo.RoomRepo;
 import com.mindre.pensionat.Services.Impl.RoomServiceImpl;
 import com.mindre.pensionat.events.Event;
+import com.mindre.pensionat.events.EventService;
+import com.mindre.pensionat.events.RoomCleaned;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -18,12 +20,16 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+
 @SpringBootTest
 @Transactional
 public class RoomServiceTest {
 
     @Autowired
     RoomServiceImpl roomService;
+
+    @Autowired
+    EventService eventService;
 
     @Autowired
     RoomRepo roomRepo;
@@ -33,6 +39,10 @@ public class RoomServiceTest {
 
     @BeforeEach
     public void setUp() {
+
+        roomRepo.deleteAll();
+        eventRepo.deleteAll();
+
         Room room1 = new Room();
         room1.setRoomType("Single");
         room1.setRoomSize(1);
@@ -45,9 +55,22 @@ public class RoomServiceTest {
         room2.setAmountOfBeds(2);
         roomRepo.save(room2);
 
+        Event event1 = new Event();
+        event1.setEmployee("Martin");
+        event1.setType("RoomCleaned");
+        event1.setRoom(room1);
+        eventRepo.save(event1);
+
+        Event event2 = new Event();
+        event2.setEmployee("Martin");
+        event2.setType("DoorOpened");
+        event2.setRoom(room2);
+        eventRepo.save(event2);
+
     }
     @Test
     public void testFindRoomById() {
+
         Room room1 = roomRepo.findAll().get(0);
         RoomDto roomDto = roomService.findRoomById(room1.getId());
         assertNotNull(roomDto);
@@ -58,20 +81,42 @@ public class RoomServiceTest {
     }
 
     @Test
+    @Transactional
+
     public void testFindAllRooms() {
+
         List<RoomDto> roomDtoList = roomService.findAllRooms();
-
-
-
-
+        assertNotNull(roomDtoList);
         assertEquals(2, roomDtoList.size());
 
+        RoomDto room1 = roomDtoList.get(0);
+        RoomDto room2 = roomDtoList.get(1);
 
+        assertEquals("Single", room1.getRoomType());
+        assertEquals("Double", room2.getRoomType());
 
-
+        assertEquals(1, room1.getRoomSize());
+        assertEquals(2, room2.getRoomSize());
     }
 
+@Test
+    public void findEventByRoomId() {
 
+        Long roomId = roomRepo.findAll().get(1).getId();
+        List<Event> events = eventRepo.findByRoomId(roomId);
+        List<Event> event = eventService.findEventByRoomId(roomId);
+
+        Event event1 = event.get(0);
+
+        assertNotNull(events);
+        assertNotNull(event);
+        assertEquals(1, event.size());
+        assertEquals(1, events.size());
+        assertEquals("Martin", event1.getEmployee());
+
+
+
+}
 
 
 }
